fc90123e2275b529b72abe105a7b0fd3f32d55a2
Tafadzwa Pasipanodya <tmpasipanodya@gmail.com>
2022-01-16 13:24:39 -0500

    c

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,7 +22,7 @@ repositories {
 		name = "JFrog"
 		url = uri("https://tmpasipanodya.jfrog.io/artifactory/releases")
 		credentials {
-			username = System.getenv("ARTIFACTORY_USER")
+			username = System.getenv("ARTIFACTORY_USERNAME")
 			password = System.getenv("ARTIFACTORY_PASSWORD")
 		}
 	}
@@ -36,7 +36,7 @@ dependencies {
 	api("org.slf4j:slf4j-simple:1.7.33")
 	api("com.fasterxml.jackson.module:jackson-module-kotlin:2.13.1")
 	api("com.fasterxml.jackson.datatype:jackson-datatype-jsr310:2.13.1")
-	testImplementation("io.taff:spek-expekt:0.6.4")
+	testImplementation("io.taff:spek-expekt:0.7.0")
 	testImplementation(enforcedPlatform("org.junit:junit-bom:5.8.2"))
 	testImplementation("com.apurebase:kgraphql:0.17.14")
 	testImplementation("io.javalin:javalin:4.3.0")
